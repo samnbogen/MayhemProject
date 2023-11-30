60d06c1964337fac470128a8c735574713c20a22
Sam <samantha.bogen@edu.sait.ca>
2023-11-30 14:15:57 -0700

    adding scrollable to start

--- src/screens/Start.jsx
+++ src/screens/Start.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import { SafeAreaView, TouchableOpacity, Text, View, StyleSheet, Image } from 'react-native';
+import { SafeAreaView, TouchableOpacity, Text, View, StyleSheet, ScrollView } from 'react-native';
 import MainLayout from '../layouts/MainLayout';
 
 const styles = StyleSheet.create({
     text: {
         color: '#dc7426', 
         textAlign: 'center',
-        fontSize: 15,
+        fontSize: 20,
         fontFamily: 'fantasy',
         paddingBottom: 10,
     },
@@ -40,22 +40,39 @@ const styles = StyleSheet.create({
 function Start({navigation}) {
     return (
         <MainLayout>
-            <SafeAreaView>
+            <ScrollView>
                 <View style={styles.container}>
-                    <Text style={styles.text}>You find yourself in the woods, waking up with no recollection of how you got there.</Text>
-                    <Text style={styles.text}> The air is cold and silent, and you can't see anything but trees around you.</Text>
-                    <Text style={styles.text}>Your objectives are clear: find a way out of the woods, and find out what happened to you.</Text>
-                    <Text style={styles.text}>Will you be able to escape or will <Text style={styles.mayhem}>MAYHEM</Text> find you?</Text>
-                    <Text style={styles.text}>The choice is yours...</Text>
+                    <Text style={styles.text}>
+                        It was great of your friend to book a cabin to spend the long weekend at, it would have been better if they bothered to find one that had internet, or even a television. Luckily you thought to bring a few bottles vodka and your roommates book of spells for a laugh. 
+                    </Text>
+                    <Text style={styles.text}>
+                        It was just supposed to be a joke. 
+                    </Text>
+                    <Text style={styles.text}>
+                        Mayhem wasn't supposed to be real, it was supposed to be nothing more than an urban legend. You had laughed when your friends suggested the ritual to summon the spirit of Mayhem. 
+                    </Text>
+                    <Text style={styles.text}>
+                        But you aren't laughing now. 
+                    </Text>
+                    <Text style={styles.text}>
+                        Not with the head of your friend rolls across the floor, your weekend officially ruined. Mayhem's eyes burn bright from across the room as if they were small portals giving you a glimpse of hell itself. The legends say it has the night to kill all those who summoned it.
+                    </Text>
+                    <Text style={styles.text}>
+                        Will you be able to escape or will <Text style={styles.mayhem}>MAYHEM</Text> find you?
+                    </Text>
+                    <Text style={styles.text}>
+                        The choice is yours...
+                    </Text>
+                
 
                 <View style={styles.buttonContainer}>
                     <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('game')}>
                         <Text style={styles.buttonText}>Continue</Text>
                     </TouchableOpacity>
-                </View>
-                </View>
+                </View>  
+                </View>              
 
-            </SafeAreaView>
+            </ScrollView>
         </MainLayout>
     );
 }
